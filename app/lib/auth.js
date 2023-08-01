8da892557260ad2b867f5a539ff1135259141050
Casey Capetola <caseycapetola@gmail.com>
2023-08-01 10:06:22 -0400

    Configured auth, added new favicon, and edited golbal layout

---
+++ app/lib/auth.js
@@ -0,0 +1,30 @@
+import SpotifyProvider from "next-auth/providers/spotify";
+
+export const authConfig = {
+  providers: [
+    SpotifyProvider({
+      clientID: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope:
+            "user-read-private user-read-email user-top-read playlist-modify-public playlist-modify-private",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.id = profile.id;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken;
+      session.id = token.id;
+      return session;
+    },
+  },
+};
